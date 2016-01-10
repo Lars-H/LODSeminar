2bae4fdc43f156bdbb0b3d4f4b072a23ca2d5353
Nico Haubner <NicoH123@users.noreply.github.com>
2016-01-10 11:38:48 +0100

    Add exception handling.

--- Cepler/scripts/helper/conversion.py
+++ Cepler/scripts/helper/conversion.py
@@ -2,60 +2,59 @@ from properties import Mapping
 from units import MassUnits, DistanceUnits, MonetaryUnits
 import sys
 
-class Conversions:
 
-	def convert(self, orig_value, orig_unit, quantity):
-		norm_value = None
-		if (quantity == Mapping.WEIGHT):
-			norm_value = self.convertToKilogram(orig_value, orig_unit)
-		elif (quantity == Mapping.DISTANCE):
-			norm_value = self.convertToMeter(orig_value, orig_unit)
-		elif (quantity == Mapping.COST):
-			norm_value = self.convertToDollar(orig_value, orig_unit)
-		else:
-			sys.exit('ERROR: Conversion failed because quantity was not properly set.')
-	
-		return norm_value
-	
-	def convertToKilogram(self, orig_value, orig_unit):
-		orig_value_parsed = float(orig_value)
-		norm_value = None
-		if orig_unit == MassUnits.TON.value:
-			norm_value = 1000*orig_value
-		elif orig_unit == MassUnits.KILOGRAM.value:
-			norm_value = orig_value
-		elif orig_unit == MassUnits.GRAM.value:
-			norm_value = orig_value/1000
-		else:
-			sys.exit('ERROR: Conversion to kilogram failed.')
+def convert(orig_value, orig_unit, quantity):
+	norm_value = None
+	if (quantity == Mapping.WEIGHT):
+		norm_value = convertToKilogram(orig_value, orig_unit)
+	elif (quantity == Mapping.DISTANCE):
+		norm_value = convertToMeter(orig_value, orig_unit)
+	elif (quantity == Mapping.COST):
+		norm_value = convertToDollar(orig_value, orig_unit)
+	else:
+		raise RuntimeError('Conversion failed because quantity was not properly set.')
 
-		print("Value converted to base unit kilogram: " + str(norm_value))
-		return norm_value
+	return norm_value
 
-	def convertToMeter(self, orig_value, orig_unit):
-		orig_value_parsed = float(orig_value)
-		norm_value = None
-		if orig_unit == DistanceUnits.KILOMETER.value:
-			norm_value = 1000*orig_value
-		elif orig_unit == DistanceUnits.METER.value:
-			norm_value = orig_value
-		elif orig_unit == DistanceUnits.CENTIMETER.value:
-			norm_value = orig_value/100
-		else:
-			sys.exit('ERROR: Conversion to meter failed.')
+def convertToKilogram(orig_value, orig_unit):
+	orig_value_parsed = float(orig_value)
+	norm_value = None
+	if orig_unit == MassUnits.TON.value:
+		norm_value = 1000*orig_value
+	elif orig_unit == MassUnits.KILOGRAM.value:
+		norm_value = orig_value
+	elif orig_unit == MassUnits.GRAM.value:
+		norm_value = orig_value/1000
+	else:
+		raise RuntimeError('Conversion to kilogram failed.')
 
-		print("Value converted to base unit meter: " + str(norm_value))
-		return norm_value
-	
-	def convertToDollar(self, orig_value, orig_unit):
-		orig_value_parsed = float(orig_value)
-		norm_value = None
-		if orig_unit == MonetaryUnits.EURO.value:
-			norm_value = 1.0851*orig_value
-		elif orig_unit == MonetaryUnits.DOLLAR.value:
-			norm_value = orig_value
-		else:
-			sys.exit('ERROR: Conversion to meter failed.')
+	print("Value converted to base unit kilogram: " + str(norm_value))
+	return norm_value
 
-		print("Value converted to base unit US dollar: " + str(norm_value))
-		return norm_value
+def convertToMeter(orig_value, orig_unit):
+	orig_value_parsed = float(orig_value)
+	norm_value = None
+	if orig_unit == DistanceUnits.KILOMETER.value:
+		norm_value = 1000*orig_value
+	elif orig_unit == DistanceUnits.METER.value:
+		norm_value = orig_value
+	elif orig_unit == DistanceUnits.CENTIMETER.value:
+		norm_value = orig_value/100
+	else:
+		raise RuntimeError('Conversion to meter failed.')
+
+	print("Value converted to base unit meter: " + str(norm_value))
+	return norm_value
+
+def convertToDollar(orig_value, orig_unit):
+	orig_value_parsed = float(orig_value)
+	norm_value = None
+	if orig_unit == MonetaryUnits.EURO.value:
+		norm_value = 1.0851*orig_value
+	elif orig_unit == MonetaryUnits.DOLLAR.value:
+		norm_value = orig_value
+	else:
+		raise RuntimeError('Conversion to meter failed.')
+
+	print("Value converted to base unit US dollar: " + str(norm_value))
+	return norm_value
--- Cepler/scripts/helper/factor.py
+++ Cepler/scripts/helper/factor.py
@@ -5,8 +5,13 @@ class Factor:
 	# a division factor which is partly dependent on the
 	# size of the input and partly random.
 	def getFactor(self, norm_value):
-		norm_value_parsed = float(norm_value)
-		# TODO insert intelligence that depends on size of input
-		# TODO Wahrscheinlichkeitsverteilung s. Lars (kleinere wahrscheinlicher)
-		factor = random.randrange(1, 100, 2)
+		
+		try:
+			norm_value_parsed = float(norm_value)
+			# TODO insert intelligence that depends on size of input
+			# TODO Wahrscheinlichkeitsverteilung s. Lars (kleinere wahrscheinlicher)
+			factor = random.randrange(1, 100, 2)
+		except Exception:
+			raise RuntimeError("Factor calculation failed!")
+		
 		return factor
