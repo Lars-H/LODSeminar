c8802bd8e081dc49e36e3e9145897444e8f0b6b1
BenHeadikt <benediktkoe@web.de>
2016-01-10 11:46:37 +0100

    Properties aufgeräumt

--- Cepler/scripts/wrapper/worldbank/wbProperties.py
+++ Cepler/scripts/wrapper/worldbank/wbProperties.py
@@ -18,39 +18,3 @@ wbIndicators = [nationalIncome, nationalSavings, weaponExports, weaponImports, f
 
 #Used years
 wbAllowedYears = ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
-
-#---------
-#Prefixes
-dbo = "http://dbpedia.org/ontology/"
-dbp = "http://dbpedia.org/property/"
-
-#Weight Properties
-weight = dbo + "weight"
-mass = dbo + "mass"
-pMass = dbp + "mass"
-pWeight = dbp + "weight"
-weightProperties = [weight , mass, pMass, pWeight]
-
-#Monetary Properties
-#cost = dbo + "cost"
-#pCost = dbp +"cost"
-pPrice = dbp + "price"
-#costProperties = [ pPrice]
-
-#Distance Properties
-length = dbo + "length"
-distance = dbo + "distance"
-height = dbo + "height"
-pLength = dbp + "length"
-pDistance = dbp + "distance"
-
-distanceProperties = [length, distance,height,  pLength, pDistance]
-
-
-
-#Types
-#Not used
-animal = dbo + "Animal"
-person = dbo + "Person"
-transportation = dbo + "MeanOfTransportation"
-
