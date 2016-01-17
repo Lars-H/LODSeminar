e48a58c45a95abbec83822620941bad6fb6fbaa9
Lars-H <larshelin@Larss-MBP.fritz.box>
2016-01-17 15:40:52 +0100

    Added Cipher
    Added Cipher for encryption

---
+++ Cepler/scripts/helper/cipher.py
@@ -0,0 +1,25 @@
+from itertools import cycle
+
+ALPHA = 'abcdefghijklmnopqrstuvwxyz&=0123456789'
+def encrypt(key, plaintext):
+    """Encrypt the string and return the ciphertext"""
+    pairs = zip(plaintext, cycle(key))
+    result = ''
+
+    for pair in pairs:
+        total = reduce(lambda x, y: ALPHA.index(x) + ALPHA.index(y), pair)
+        result += ALPHA[total % 38]
+
+    return result.lower()
+
+def decrypt(key, ciphertext):
+    """Decrypt the string and return the plaintext"""
+    pairs = zip(ciphertext, cycle(key))
+    result = ''
+
+    for pair in pairs:
+        total = reduce(lambda x, y: ALPHA.index(x) - ALPHA.index(y), pair)
+        result += ALPHA[total % 38]
+
+    return result
+
