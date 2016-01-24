96a086fc766cea04f357ecaf5bcbc515378064e9
BenHeadikt <benediktkoe@web.de>
2016-01-24 14:32:25 +0100

    testfile aktualisiert

--- Cepler/scripts/wrapper/worldbank/testWBRDF.py
+++ Cepler/scripts/wrapper/worldbank/testWBRDF.py
@@ -13,15 +13,15 @@ wbWrapper1 = wbWrapper();
 #print wbWrapper1.getDBPic("<http://dbpedia.org/resource/Equatorial_Guinea>")
 
 
-#testresult = wbWrapper1.buildQuery(Mapping.COST, 45000000000, 4500000000)
+rdfResult = wbWrapper1.getResults(Mapping.COST, 2500, 1000)
 
-rdfResult = wbWrapper1.getResults(Mapping.COST, 2, 4) 
+#rdfResult = wbWrapper1.getResults(Mapping.COST, 2, 4) 
 
 
 if rdfResult is not None:
 
 
-	rdfResult.serialize(destination='output.txt', format='turtle')
+	#rdfResult.serialize(destination='output.txt', format='turtle')
 
 	for stmt in rdfResult:
 		pprint.pprint(stmt)
